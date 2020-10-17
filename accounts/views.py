aebfa5d430de5ce997eb8e4d186ef5bc7df85b59
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-10-17 15:37:30 +0530

    Render HTML Login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def login(request):
-    return HttpResponse("Login page")
+    return render(request, 'login.html')
