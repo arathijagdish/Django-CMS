39dcc8d94fe12d4c76eb6b0b185411074f2b6071
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-10-17 15:58:41 +0530

    Render login form

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'login.html', {'form': AuthenticationForm()})
