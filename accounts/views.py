7a484bcabc2aeca3db946707985993ce545f0b6e
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-10-20 14:44:38 +0530

    Created registration page

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 # Create your views here.
 def login(request):
     return render(request, 'login.html', {'form': AuthenticationForm()})
+
+def register(request):
+    return render(request, 'register.html', {'form': UserCreationForm()})
