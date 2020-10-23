c7ffa708561e853fb18ec56ec44bf533485bfebe
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-10-23 15:48:15 +0530

    Removed extra / from url

--- staff/urls.py
+++ staff/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='staff_home'),
-    path('/post/new', new_post, name='staff_post_creation'),
+    path('post/new', new_post, name='staff_post_creation'),
 ]
